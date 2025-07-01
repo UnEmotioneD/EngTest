d0a60b30c1c878f3c9022aeeea256211f5b85606
UnEmotioneD <blackeagle10@icloud.com>
2025-07-01 14:24:48 +0900

    Update format with google-java-format

--- src/kr/or/iei/model/vo/Word.java
+++ src/kr/or/iei/model/vo/Word.java
@@ -1,47 +1,46 @@
 package kr.or.iei.model.vo;
 
 public class Word {
-	private String word;
-	private String def1;
-	private String def2;
+  private String word;
+  private String def1;
+  private String def2;
 
-	public Word(String word, String def1, String def2, int j) {
-		super();
-	}
+  public Word(String word, String def1, String def2, int j) {
+    super();
+  }
 
-	public Word(String word, String def1, String def2) {
-		this.word = word;
-		this.def1 = def1;
-		this.def2 = def2;
-	}
+  public Word(String word, String def1, String def2) {
+    this.word = word;
+    this.def1 = def1;
+    this.def2 = def2;
+  }
 
-	public String getWord() {
-		return word;
-	}
+  public String getWord() {
+    return word;
+  }
 
-	public void setWord(String word) {
-		this.word = word;
-	}
+  public void setWord(String word) {
+    this.word = word;
+  }
 
-	public String getDef1() {
-		return def1;
-	}
+  public String getDef1() {
+    return def1;
+  }
 
-	public void setDef1(String def1) {
-		this.def1 = def1;
-	}
+  public void setDef1(String def1) {
+    this.def1 = def1;
+  }
 
-	public String getDef2() {
-		return def2;
-	}
+  public String getDef2() {
+    return def2;
+  }
 
-	public void setDef2(String def2) {
-		this.def2 = def2;
-	}
+  public void setDef2(String def2) {
+    this.def2 = def2;
+  }
 
-	@Override
-	public String toString() {
-		return word + "\t" + def1 + "\t" + def2;
-	}
-
+  @Override
+  public String toString() {
+    return word + "\t" + def1 + "\t" + def2;
+  }
 }
--- src/kr/or/iei/viewer/EnglishViewer.java
+++ src/kr/or/iei/viewer/EnglishViewer.java
@@ -2,82 +2,77 @@ package kr.or.iei.viewer;
 
 import java.util.ArrayList;
 import java.util.Scanner;
-
 import kr.or.iei.model.vo.WordWithIndex;
 
 public class EnglishViewer {
-	Scanner sc;
+  Scanner sc;
 
-	public EnglishViewer() {
-		sc = new Scanner(System.in);
-	}
+  public EnglishViewer() {
+    sc = new Scanner(System.in);
+  }
 
-	public int menu() {
-		System.out.println("===== Study English =====");
-		System.out.println("1 Search");
-		System.out.println("2 Add new");
-		System.out.println("3 Start test");
-		System.out.println("4 Make-up exam");
-		System.out.println("5 Edit / Delete");
-		System.out.println("0 Terminate");
-		System.out.print("=> ");
-		return sc.nextInt();
-	}
+  public int menu() {
+    System.out.println("===== Study English =====");
+    System.out.println("1 Search");
+    System.out.println("2 Add new");
+    System.out.println("3 Start test");
+    System.out.println("4 Make-up exam");
+    System.out.println("5 Edit / Delete");
+    System.out.println("0 Terminate");
+    System.out.print("=> ");
+    return sc.nextInt();
+  }
 
-	public String searchViewer() {
-		System.out.println("Search Word / Cancel (w/c)");
-		System.out.print("=> ");
-		return sc.next();
-	}
+  public String searchViewer() {
+    System.out.println("Search Word / Cancel (w/c)");
+    System.out.print("=> ");
+    return sc.next();
+  }
 
-	public String addViewer(String text) {
-		System.out.println("Enter new " + text + " : ");
-		return sc.next();
-	}
+  public String addViewer(String text) {
+    System.out.println("Enter new " + text + " : ");
+    return sc.next();
+  }
 
-	public String editViewer() {
-		System.out.println("Search word to edit / delete");
-		System.out.println("Delete all / Cancel (a/c)");
-		System.out.print("=> ");
-		return sc.next();
-	}
+  public String editViewer() {
+    System.out.println("Search word to edit / delete");
+    System.out.println("Delete all / Cancel (a/c)");
+    System.out.print("=> ");
+    return sc.next();
+  }
 
-	public String startTest() {
-		System.out.print("Select to English or Korean (e/k): ");
-		return sc.next();
+  public String startTest() {
+    System.out.print("Select to English or Korean (e/k): ");
+    return sc.next();
+  }
 
-	}
+  public int random() {
+    System.out.print("Please enter the number of tests : ");
+    return sc.nextInt();
+  }
 
-	public int random() {
-		System.out.print("Please enter the number of tests : ");
-		return sc.nextInt();
+  public String randomTest() {
+    System.out.print("Answer : ");
+    return sc.next();
+  }
 
-	}
+  public char editOrDelete() {
+    return sc.next().charAt(0);
+  }
 
-	public String randomTest() {
-		System.out.print("Answer : ");
-		return sc.next();
+  public int searchView(ArrayList<WordWithIndex> list) {
+    for (int i = 0; i < list.size(); i++) {
+      System.out.printf("%2d %-13s\t", i + 1, list.get(i).getWord());
+      if ((i + 1) % 3 == 0) {
+        System.out.println();
+      }
+    }
+    System.out.print("\nChoose index : ");
+    return sc.nextInt();
+  }
 
-	}
-
-	public char editOrDelete() {
-		return sc.next().charAt(0);
-	}
-
-	public int searchView(ArrayList<WordWithIndex> list) {
-		for (int i = 0; i < list.size(); i++) {
-			System.out.printf("%2d %-13s\t", i + 1, list.get(i).getWord());
-			if ((i + 1) % 3 == 0) {
-				System.out.println();
-			}
-		}
-		System.out.print("\nChoose index : ");
-		return sc.nextInt();
-	}
-
-	public void showChosenIndex(int chosenIndex, ArrayList<WordWithIndex> list) {
-		System.out.print(list.get(chosenIndex).toString());
-		System.out.println();
-	}
-
+  public void showChosenIndex(int chosenIndex, ArrayList<WordWithIndex> list) {
+    System.out.print(list.get(chosenIndex).toString());
+    System.out.println();
+  }
 }
